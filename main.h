409d47654b9315d178331af24f2c0e015e10768c
Mulongovictor <mulongovibu@gmail.com>
2023-09-17 23:58:03 +0300

    main.h file created

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int_printf(const char *format, ...);
+
+#endif
