6b3658a8740d3b2c78d6dd948417b525ff56823d
Mulongovictor <mulongovibu@gmail.com>
2023-09-18 00:47:48 -0700

    The printf function file

---
+++ printf_func.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - is function that outputs on the std output
+ * @format: is the string that is parsed to the function
+ *
+ * Returns: 0 Always (SUCCESS)
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
