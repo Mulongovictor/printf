414450b2fd4ab6ebe36c63fb046368c91a8d453a
Mulongovictor <mulongovibu@gmail.com>
2023-09-18 01:42:38 -0700

    printf_func.c with the program

--- printf_func.c
+++ printf_func.c
@@ -9,5 +9,53 @@
 
 int _printf(const char *format, ...)
 {
+	int countchar = 0;
+	va_list formatchar;
 
+	if (format == NULL)
+		return (-1);
+
+	va_start(formatchar, format);
+
+	while(*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			countchar++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				countchar++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(formatchar, int);
+				write(1, &c, 1);
+				countchar++;
+			}
+			else if (*format == 's')
+			{
+				char * strng = va_arg(formatchar, char*);
+				strglen = 0;
+				int i;
+				for (i = 0; strng[i] != '\0'; i++)
+				{
+					strglen++;
+				}
+				write(1, strng, strglen);
+				countchar += strglen;
+			}
+
+		}
+		format++;
+	}
+	va_end(formatchar);
+	return (countchar);
 }
