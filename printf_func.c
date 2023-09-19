65562ab4c13448b26f1152623183093ddcd3145b
Mulongovictor <mulongovibu@gmail.com>
2023-09-19 03:18:46 -0700

    printf_func.c edited with two more if conditions but one removed

--- printf_func.c
+++ printf_func.c
@@ -18,6 +18,11 @@ int _printf(const char *format, ...)
 
 	va_start(formatchar, format);
 
+	if (!format ||(format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	while (*format)
 	{
 		if (*format != '%')
